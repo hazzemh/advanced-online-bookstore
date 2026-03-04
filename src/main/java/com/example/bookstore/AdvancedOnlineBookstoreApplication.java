31b1cb8f3d211c4b441b8538bf5d8bdd7a828014
hazzemh <hazemhossam12122001@gmail.com>
2026-03-04 16:31:37 +0200

    implement the setup and modular structure for the project

---
+++ src/main/java/com/example/bookstore/AdvancedOnlineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedOnlineBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedOnlineBookstoreApplication.class, args);
+    }
+
+}
