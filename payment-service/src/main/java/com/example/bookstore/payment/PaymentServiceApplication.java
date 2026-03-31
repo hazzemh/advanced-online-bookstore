65bd8b75ae6f3e5234c9e44e4a8dc57779928b85
hazzemh <hazemhossam12122001@gmail.com>
2026-03-31 03:15:32 +0200

    Fix payment-service component scanning

--- payment-service/src/main/java/com/example/bookstore/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/example/bookstore/payment/PaymentServiceApplication.java
@@ -3,10 +3,9 @@ package com.example.bookstore.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.bookstore")
 public class PaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 }
-
